972f9326de9541a9e9bdf24cbe957c7933add8d7
Yashaha <yashaha@foxmail.com>
2018-08-23 16:43:35 +0800

    use vuex

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -5,14 +5,18 @@
 				<div class="title border-topbottom">当前城市</div>
 				<div class="button-list">
 					<div class="button-wrapper">
-						<div class="button">北京</div>
+						<div class="button">{{this.currentCity}}</div>
 					</div>
 				</div>
 			</div>
 			<div class="area">
 				<div class="title border-topbottom">热门城市</div>
 				<div class="button-list">
-					<div class="button-wrapper" v-for="item of hotCities" :key="item.id">
+					<div class="button-wrapper"
+						v-for="item of hotCities"
+						:key="item.id"
+						@click="handleCityClick(item.name)"
+					>
 						<div class="button">{{item.name}}</div>
 					</div>
 				</div>
@@ -20,7 +24,13 @@
 			<div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
 				<div class="title border-topbottom">{{key}}</div>
 				<div class="item-list">
-					<div class="item border-bottom" v-for="city of item" :key="city.id">{{city.name}}</div>
+					<div class="item border-bottom"
+						v-for="city of item"
+						:key="city.id"
+						@click="handleCityClick(city.name)"
+					>
+						{{city.name}}
+					</div>
 				</div>
 			</div>
 		</div>
@@ -29,12 +39,25 @@
 
 <script>
 import Bscroll from 'better-scroll'
+import { mapState,mapMutations } from 'vuex'
 export default {
 	name: 'CityList',
 	props: {
 		hotCities: Array,
 		cities: Object,
 		letter: String
+	},
+	computed: {
+		...mapState({
+			currentCity: 'city'
+		})
+	},
+	methods: {
+		handleCityClick (city) {
+			this.changeCity(city)
+			this.$router.push('/')
+		},
+		...mapMutations(['changeCity'])
 	},
 	mounted () {
 		this.scroll = new Bscroll(this.$refs.wrapper)
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -11,10 +11,11 @@
 </template>
 
 <script>
+	import { mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
-  props: {
-  	city: String
+  computed: {
+  	...mapState(['city'])
   }
 }
 </script>
@@ -44,7 +45,8 @@ export default {
 			border-radius: .1rem
 			color: #ccc
 		.header-right
-			width:1.24rem
+			min-width: 1.04rem
+			padding: 0 .1rem
 			float: right
 			text-align: center
 			color: #fff
